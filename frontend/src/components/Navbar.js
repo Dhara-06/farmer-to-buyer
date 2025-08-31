a9e4b03386a6b49fc49f216861e59c0da3fcb4d6
Dhara-06 <dharani2006dgl@gmail.com>
2025-08-31 10:51:53 +0530

    navbar changed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,102 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 
-function Navbar() {
+function AppNavbar() {
+  // State for each dropdown
+  const [showFarmers, setShowFarmers] = useState(false);
+  const [showBuyers, setShowBuyers] = useState(false);
+  const [showAdmin, setShowAdmin] = useState(false);
+
+  // Close all dropdowns
+  const closeAll = () => {
+    setShowFarmers(false);
+    setShowBuyers(false);
+    setShowAdmin(false);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-      <div className="container-fluid">
-        <Link className="navbar-brand fw-bold" to="/">
+    <Navbar bg="success" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/" className="fw-bold">
           <i className="fas fa-leaf"></i> SmartAgri
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav className="ms-auto">
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
             {/* Farmers Dropdown */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-              >
-                <i className="fas fa-tractor"></i> Farmers
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/farmer/register">
-                    Register
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/farmer/login">
-                    Login
-                  </Link>
-                </li>
-              </ul>
-            </li>
+            <NavDropdown
+              title={<><i className="fas fa-tractor"></i> Farmers</>}
+              id="farmers-dropdown"
+              show={showFarmers}
+              onToggle={(isOpen) => {
+                closeAll();
+                setShowFarmers(isOpen);
+              }}
+            >
+              <NavDropdown.Item as={Link} to="/farmer/register" onClick={closeAll}>
+                Register
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/farmer/login" onClick={closeAll}>
+                Login
+              </NavDropdown.Item>
+            </NavDropdown>
 
             {/* Buyers Dropdown */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-              >
-                <i className="fas fa-shopping-cart"></i> Buyers
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/buyer/register">
-                    Register
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/buyer/login">
-                    Login
-                  </Link>
-                </li>
-              </ul>
-            </li>
+            <NavDropdown
+              title={<><i className="fas fa-shopping-cart"></i> Buyers</>}
+              id="buyers-dropdown"
+              show={showBuyers}
+              onToggle={(isOpen) => {
+                closeAll();
+                setShowBuyers(isOpen);
+              }}
+            >
+              <NavDropdown.Item as={Link} to="/buyer/register" onClick={closeAll}>
+                Register
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/buyer/login" onClick={closeAll}>
+                Login
+              </NavDropdown.Item>
+            </NavDropdown>
 
             {/* Admin Dropdown */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-              >
-                <i className="fas fa-user-shield"></i> Admin
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/admin/login">
-                    Login
-                  </Link>
-                </li>
-              </ul>
-            </li>
+            <NavDropdown
+              title={<><i className="fas fa-user-shield"></i> Admin</>}
+              id="admin-dropdown"
+              show={showAdmin}
+              onToggle={(isOpen) => {
+                closeAll();
+                setShowAdmin(isOpen);
+              }}
+            >
+              <NavDropdown.Item as={Link} to="/admin/login" onClick={closeAll}>
+                Login
+              </NavDropdown.Item>
+            </NavDropdown>
 
             {/* Blog */}
-            <li className="nav-item">
-              <Link className="nav-link" to="/blog">
-                <i className="fas fa-blog"></i> Blog
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+            <Nav.Link as={Link} to="/blog" onClick={closeAll}>
+              <i className="fas fa-blog"></i> Blog
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default AppNavbar;
